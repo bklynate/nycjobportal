174ab7387415b4f3e30b02f52d4ad90a9a251049
bklynate <nathanielcfa@gmail.com>
2020-01-29 23:27:39 -0500

    feat: add basic display of job search results

--- client/src/components/JobResultItem.js
+++ client/src/components/JobResultItem.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const JobResultItem = props => {
-  console.log('JobResultItem::', props);
+  const {
+    agency,
+    business_title,
+    civil_service_title,
+    division_work_unit,
+    full_time_part_time_indicator,
+    job_category,
+    work_location,
+  } = props.data;
   return (
-    <div>
-      <h1>HELLO JobResultItem</h1>
+    <div style={{ border: '1px solid red', marginBottom: '10px' }}>
+      <p>{agency}</p>
+      <p>{business_title}</p>
+      <p>{civil_service_title}</p>
+      <p>{division_work_unit}</p>
+      <p>{full_time_part_time_indicator}</p>
+      <p>{job_category}</p>
+      <p>{work_location}</p>
     </div>
   );
 };
--- client/src/components/JobResultsList.js
+++ client/src/components/JobResultsList.js
@@ -4,13 +4,11 @@ import { connect } from 'react-redux';
 
 const renderResultsList = props => {
   const { data: jobs = [] } = props.jobs || {};
-  console.log('JOBS::', jobs);
 
   return jobs.map((job, index) => <JobResultItem key={index} data={job} />);
 };
 
 const JobsResultsList = props => {
-  console.log('Here are the props::', props);
   return (
     <div>
       <h1>Hi</h1>
@@ -21,7 +19,6 @@ const JobsResultsList = props => {
 
 const mapStateToProps = state => {
   const { jobs } = state || {};
-  console.log('Here is this state mapstatetoprops::', state);
   return { jobs };
 };
 
