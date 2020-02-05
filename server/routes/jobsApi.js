65a55dfe493354b14bac67be74781b0100bcf537
Peter Han <peterhan@Peters-MacBook-Pro.local>
2020-02-04 22:00:00 -0500

    feat: wrap api calls in try/catch

--- server/routes/jobsApi.js
+++ server/routes/jobsApi.js
@@ -2,22 +2,30 @@ import axios from 'axios';
 import keys from '../config/dev';
 
 export default app => {
-  app.get('/api/alljobs', async (request, response) => {
-    const apiResponse = await axios(
-      `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${keys.APIKEY}`
-    );
-    const { data } = apiResponse;
-    response.send({ data });
+  app.get('/api/alljobs', async (req, res) => {
+    try {
+      const response = await axios(
+        `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${keys.APIKEY}`
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   });
 
-  app.post('/api/getjobbykeyword', async (request, response) => {
-    const { keyword = 'javascript' } = request.body;
-    const apiResponse = await axios(
-      `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${keys.APIKEY}&&${encodeURI(
-        `$query=select * search '${keyword}' limit 100&$$query_timeout_seconds=60`
-      )}`
-    );
-    const { data = {} } = apiResponse || {};
-    response.send({ data });
+  app.post('/api/getjobbykeyword', async (req, res) => {
+    try {
+      const { keyword = 'javascript' } = request.body;
+      const response = await axios(
+        `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${
+          keys.APIKEY
+        }&&${encodeURI(
+          `$query=select * search '${keyword}' limit 100&$$query_timeout_seconds=60`
+        )}`
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   });
 };
