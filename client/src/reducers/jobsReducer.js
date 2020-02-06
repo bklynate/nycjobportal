d0701a6c08c4d8b876ccce056eeb9652d79a37e4
Peter Han <han.peter92@gmail.com>
2020-02-05 21:04:26 -0500

    feat: add jobs by keyword reducer

--- client/src/reducers/jobsReducer.js
+++ client/src/reducers/jobsReducer.js
@@ -2,11 +2,15 @@ import {
   FETCH_ALL_JOBS_START,
   FETCH_ALL_JOBS_SUCCESS,
   FETCH_ALL_JOBS_FAILED,
+  FETCH_JOBS_BY_KEYWORD_START,
+  FETCH_JOBS_BY_KEYWORD_SUCCESS,
+  FETCH_JOBS_BY_KEYWORD_FAILED
 } from './../actions/types';
 
 const initialState = {
   isLoading: false,
   allJobs: [],
+  searchJobKeywords: [],
   error: null,
 };
 
@@ -17,6 +21,12 @@ export default function(state = initialState, action) {
     case FETCH_ALL_JOBS_SUCCESS:
       return { ...state, isLoading: false, allJobs: action.payload };
     case FETCH_ALL_JOBS_FAILED:
+      return { ...state, isLoading: false, error: action.payload };
+    case FETCH_JOBS_BY_KEYWORD_START:
+      return { ...state, isLoading: true };
+    case FETCH_JOBS_BY_KEYWORD_SUCCESS:
+      return { ...state, isLoading: false, searchJobKeywords: action.payload };
+    case FETCH_JOBS_BY_KEYWORD_FAILED:
       return { ...state, isLoading: false, error: action.payload };
     default:
       return state;
