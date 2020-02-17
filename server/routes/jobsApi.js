cde91e393a3ca53d19b6e6d60ba418bbe1b304f7
Jonathan Chin <jonathankchin@gmail.com>
2020-02-16 21:15:08 -0500

    refactor: abstract api calls into module

--- server/routes/jobsApi.js
+++ server/routes/jobsApi.js
@@ -1,12 +1,9 @@
-import axios from 'axios';
-import keys from '../config/dev';
+import { getJobs } from './getJobs.js'
 
 export default app => {
   app.get('/api/alljobs', async (req, res) => {
     try {
-      const response = await axios(
-        `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${keys.APIKEY}`
-      );
+      const response = await getJobs();
       res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ message: error });
@@ -16,13 +13,7 @@ export default app => {
   app.post('/api/getjobbykeyword', async (req, res) => {
     try {
       const { keyword } = req.body;
-      const response = await axios(
-        `https://data.cityofnewyork.us/resource/kpav-sd4t.json/?$$app_token=${
-          keys.APIKEY
-        }&&${encodeURI(
-          `$query=select * search '${keyword}' limit 100&$$query_timeout_seconds=60`
-        )}`
-      );
+      const response = await getJobs(keyword);
       res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ message: error });
