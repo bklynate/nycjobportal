38fc367232dfa152d67127dfa419e87be51fd6b4
Jonathan Chin <jonathankchin@gmail.com>
2020-02-17 18:41:13 -0500

    chore: add api keys to prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,6 +3,8 @@
 export default {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   mongoURI: process.env.MONGODB_URI,
 };
