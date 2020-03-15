5f3f5e85f08a16f26d4dc8302daa5a3e27f4f4ee
bklynate <nathanielcfa@gmail.com>
2020-03-15 14:09:53 -0400

    fix: edit store.js to fix store creation bug

--- client/store.js
+++ client/store.js
@@ -1,22 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-// import { ajaxMiddleware } from './middleware';
 import reducers from './reducers';
 
 const middleware = [thunk];
 let mutableStore;
 
 if (typeof window !== 'undefined') {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   mutableStore = createStore(
     reducers,
     window.INITIAL_STATE,
-    composeEnhancers(applyMiddleware(...middleware))
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
+    )
   );
+} else {
+  mutableStore = createStore(reducers, {}, applyMiddleware(...middleware));
 }
-
-mutableStore = createStore(reducers, {}, applyMiddleware(...middleware));
 
 const store = mutableStore;
 
