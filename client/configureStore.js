6bfb9091f343a2ae3d4e70c5f4f7c252e1e64ad0
bklynate <nathanielcfa@gmail.com>
2020-03-30 00:34:24 -0400

    feat: remove commented out code

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-// import serialize from 'serialize-javascript';
 import reducers from './reducers';
 
 const middleware = [thunk];
