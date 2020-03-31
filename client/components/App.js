691ad3dbde70b14f81c7621e84e57d01f3130817
bklynate <nathanielcfa@gmail.com>
2020-03-31 02:09:36 -0400

    feat: add custom theme for chakra ui

--- client/components/App.js
+++ client/components/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ThemeProvider, Flex, Box } from '@chakra-ui/core';
+import { ThemeProvider, Flex, Box, theme } from '@chakra-ui/core';
 import { renderRoutes } from 'react-router-config';
 import * as actions from '../actions';
 
 import Header from './Header';
+
+const customTheme = {
+  ...theme,
+  fonts: {
+    body: `Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  },
+};
 
 const App = props => {
   const { route } = props;
   const { routes } = route;
   return (
     <Box maxW="1440px" mx="auto">
-      <ThemeProvider>
+      <ThemeProvider theme={customTheme}>
         <Header />
         <Flex display="flex" flexDirection="column" mt={4}>
           {renderRoutes(routes)}
