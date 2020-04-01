b16255517b3feb93b8ecd6b9f021d39055098899
Jonathan Chin <jonathankchin@gmail.com>
2020-03-31 21:59:16 -0400

    test: update tests for async/await

--- server/routes/tests/getJobs.test.js
+++ server/routes/tests/getJobs.test.js
@@ -1,9 +1,17 @@
 import { getJobs } from './../getJobs.js';
 
+const testConstants = {
+  baseUrl: 'https://data.cityofnewyork.us/resource/kpav-sd4t.json'
+}
+
 describe('getJobs()', () => {
-  it('should return data of type object', () => {
-    getJobs('test').then(data => {
-      expect(typeof data).toBe('object');
-    })
+  it('should return data of type object', async () => {
+    const data = await getJobs('test');
+    expect(typeof data).toBe('object');
+  });
+
+  it('should send request to correct url', async () => {
+    const data = await getJobs('test');
+    expect(data.config.url).toContain(testConstants.baseUrl);
   });
 });
