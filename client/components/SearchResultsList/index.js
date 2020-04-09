0355555daece35cce566bfaa9be8a22f3a89f7ef
bklynate <nathanielcfa@gmail.com>
2020-04-08 23:00:36 -0400

    feat: make SearchResultsList(formally JobsResultsList) mobile-first  responsive

---
+++ client/components/SearchResultsList/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Box } from '@chakra-ui/core';
+import JobResultItem from '../JobResultItem';
+import styles from './styles.scss';
+
+const renderResultsList = (props) => {
+  const { data: jobs = [] } = props;
+  return jobs.map((job, index) => <JobResultItem key={index} data={job} />);
+};
+
+const SearchResultsList = (props) => (
+  <Box className={styles.jobResultsListContainer} my="2rem">
+    {renderResultsList(props)}
+  </Box>
+);
+
+const mapStateToProps = (state) => {
+  const { jobs } = state || {};
+  return jobs;
+};
+
+export default connect(mapStateToProps)(SearchResultsList);
