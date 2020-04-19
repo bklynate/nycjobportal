4af78188305edcba6b723ef3e7926b8f554bd537
Peter Han <han.peter92@gmail.com>
2020-04-19 18:17:22 -0400

    test/jobsApi

--- __tests__/setupJest.js
+++ __tests__/setupJest.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 
-global.jobsBaseUrl = `https://data.cityofnewyork.us`;
+global.internalBaseUrl = 'http://localhost:5000';
+global.jobsBaseUrl = 'https://data.cityofnewyork.us';
 global.searchText = 'Intern';
