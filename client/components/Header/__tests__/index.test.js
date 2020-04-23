8c0629794d6d5908e0438e5db34ffa6a8f07b729
bklynate <nathanielcfa@gmail.com>
2020-04-23 00:54:47 -0400

    feat: add additional header tests

--- client/components/Header/__tests__/index.test.js
+++ client/components/Header/__tests__/index.test.js
@@ -6,8 +6,20 @@ expect.addSnapshotSerializer(serializer);
 
 describe('Header', () => {
   describe('optimal case', () => {
+    const { asFragment, queryByText } = render(<Header />);
+    it("should render the text 'Easily Find City Job Listings'", () => {
+      expect(queryByText('Easily Find City Job Listings')).not.toBeNull();
+    });
+
+    it("should render the text 'NYC JOB SEARCHER'", () => {
+      expect(queryByText('NYC JOB SEARCHER')).not.toBeNull();
+    });
+
+    it("should render with a badge that has the text 'BETA'", () => {
+      expect(queryByText('BETA')).not.toBeNull();
+    });
+
     it('should match snapshot', () => {
-      const { asFragment } = render(<Header />);
       expect(asFragment()).toMatchSnapshot();
     });
   });
--- setupJest.js
+++ setupJest.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { render as testRender } from '@testing-library/react';
 import { ThemeProvider, theme } from '@chakra-ui/core';
 
+const customTheme = {
+  ...theme,
+  fonts: {
+    body: `Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  },
+};
+
 require('dotenv').config();
 
 const render = Component => {
-  return testRender(<ThemeProvider theme={theme}>{Component}</ThemeProvider>);
+  return testRender(
+    <ThemeProvider theme={customTheme}>{Component}</ThemeProvider>
+  );
 };
 
 global.internalBaseUrl = 'http://localhost:5000';
