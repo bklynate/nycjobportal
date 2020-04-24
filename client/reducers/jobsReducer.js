e192beae7ceee8e36d4bc14f272f1bd30e336168
bklynate <nathanielcfa@gmail.com>
2020-04-24 00:16:05 -0400

    test: add tests for jobs reducer

--- client/reducers/jobsReducer.js
+++ client/reducers/jobsReducer.js
@@ -1,6 +1,6 @@
 import { FETCH_KEYWORD_JOBS } from '../actions/types';
 
-export default function(state = {}, action) {
+export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_KEYWORD_JOBS:
       return { ...state, ...action.payload };
