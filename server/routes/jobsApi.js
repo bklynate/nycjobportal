07cb0a567178b0e579dd40558f35eb7e3d9b6d82
bklynate <nathanielcfa@gmail.com>
2020-05-25 20:37:07 -0400

    refactor: edit naming of routes

--- server/routes/jobsApi.js
+++ server/routes/jobsApi.js
@@ -1,7 +1,7 @@
 import { getJobs } from './getJobs';
 
 export default app => {
-  app.get('/api/alljobs', async (req, res) => {
+  app.get('/api/get-all-jobs', async (req, res) => {
     try {
       const { data } = await getJobs();
       res.status(200).send(data);
@@ -10,7 +10,16 @@ export default app => {
     }
   });
 
-  app.post('/api/get-job-by-keyword', async (req, res) => {
+  app.post('/api/get-jobs-by-keyword', async (req, res) => {
+    try {
+      const { keyword } = req.body;
+      const { data } = await getJobs(keyword);
+      res.status(200).send({ data });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  });
+  app.post('/api/get-single-job-listing', async (req, res) => {
     try {
       const { keyword } = req.body;
       const { data } = await getJobs(keyword);
